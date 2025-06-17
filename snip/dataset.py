05a75e40a007515b42b29445e45c4efff4035059
Jacky Qin <60870424+qinyong123456@users.noreply.github.com>
2025-06-17 15:27:20 +0800

    Update dataset.py

--- snip/dataset.py
+++ snip/dataset.py
@@ -1,26 +1,75 @@
 import os
 import itertools
 import numpy as np
-
-import mnist
-import cifar
-
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 class Dataset(object):
     def __init__(self, datasource, path_data, **kwargs):
         self.datasource = datasource
         self.path_data = path_data
         self.rand = np.random.RandomState(9)
+        
         if self.datasource == 'mnist':
             self.num_classes = 10
-            self.dataset = mnist.read_data(self.path_data)
+            # 加载MNIST数据集
+            train_dataset = datasets.MNIST(
+                root=path_data,
+                train=True,
+                download=True,
+                transform=transforms.ToTensor()
+            )
+            test_dataset = datasets.MNIST(
+                root=path_data,
+                train=False,
+                download=True,
+                transform=transforms.ToTensor()
+            )
+            
+            # 转换为NumPy数组
+            train_images = train_dataset.data.numpy().reshape(-1, 1, 28, 28) / 255.0
+            train_labels = train_dataset.targets.numpy()
+            test_images = test_dataset.data.numpy().reshape(-1, 1, 28, 28) / 255.0
+            test_labels = test_dataset.targets.numpy()
+            
+            self.dataset = {
+                'train': {'input': train_images, 'label': train_labels},
+                'test': {'input': test_images, 'label': test_labels}
+            }
+            
         elif self.datasource == 'cifar-10':
             self.num_classes = 10
-            self.dataset = cifar.read_data(os.path.join(self.path_data, 'cifar-10-batches-py'))
+            # 加载CIFAR-10数据集
+            train_dataset = datasets.CIFAR10(
+                root=path_data,
+                train=True,
+                download=True,
+                transform=transforms.ToTensor()
+            )
+            test_dataset = datasets.CIFAR10(
+                root=path_data,
+                train=False,
+                download=True,
+                transform=transforms.ToTensor()
+            )
+            
+            # 转换为NumPy数组
+            train_images = np.transpose(np.array(train_dataset.data), (0, 3, 1, 2)) / 255.0
+            train_labels = np.array(train_dataset.targets)
+            test_images = np.transpose(np.array(test_dataset.data), (0, 3, 1, 2)) / 255.0
+            test_labels = np.array(test_dataset.targets)
+            
+            self.dataset = {
+                'train': {'input': train_images, 'label': train_labels},
+                'test': {'input': test_images, 'label': test_labels}
+            }
+            
         else:
             raise NotImplementedError
-        self.split_dataset('train', 'val', int(self.dataset['train']['input'].shape[0] * 0.1),
-            self.rand)
+            
+        # 分割训练集为训练集和验证集
+        self.split_dataset('train', 'val', int(self.dataset['train']['input'].shape[0] * 0.1), self.rand)
         self.num_example = {k: self.dataset[k]['input'].shape[0] for k in self.dataset.keys()}
         self.example_generator = {
             'train': self.iterate_example('train'),
